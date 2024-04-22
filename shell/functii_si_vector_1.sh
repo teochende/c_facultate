aa8ca0c6ccf9799d39372213995e8b0263dde18d
crchende <57460107+crchende@users.noreply.github.com>
2024-04-22 23:29:06 +0300

    Update functii_si_vector_1.sh

--- shell/functii_si_vector_1.sh
+++ shell/functii_si_vector_1.sh
@@ -1,1 +1,41 @@
+!/bin/bash
 
+
+
+creare_vect() {
+
+	i = 0;
+  echo "introduceti o valoare: "
+
+	read v[0]
+  i=$i+1 
+
+	echo "doriti sa mai introduceti o valoare (1 daca da, altceva daca nu)?"
+  read raspuns
+
+	while [ $raspuns -eq 1 ]
+  do
+    echo "introduceti o valoare: "
+    read v[i]
+    i=$i+1
+    
+    echo "doriti sa mai introduceti o valoare( 1 daca da altaceva daca nu )?"
+    read raspuns
+  done
+ 
+	n=$i
+	echo "vector final: ${v[@]}"
+  return ${v[@]}
+}
+
+
+max_vect() {
+
+	max = $1[0]
+  echo "max = $max"
+}
+
+v=`creare_vect`
+#vector=$(creare_vect)
+
+#echo ${v[@]}
