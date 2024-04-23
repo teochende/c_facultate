9cc0015d589b27a5b4f06049d16f0f6ca86b96e0
crchende <57460107+crchende@users.noreply.github.com>
2024-04-23 23:25:51 +0300

    Update functii_si_vector_1.sh

--- shell/functii_si_vector_1.sh
+++ shell/functii_si_vector_1.sh
@@ -1,41 +1,68 @@
-!/bin/bash
+#!/bin/bash
 
+#
+# Site foarte bun:
+# https://www.shellscript.sh/functions.html
+#
 
+# utilizare read:
+# https://www.geeksforgeeks.org/bash-scripting-how-to-read-a-file-line-by-line/?ref=ml_lbp
+
+# https://www.geeksforgeeks.org/bash-scripting-functions/?ref=ml_lbp
 
 creare_vect() {
+    i=0
+    echo -n "introduceti o valoare: "
 
-	i = 0;
-  echo "introduceti o valoare: "
+    read v[0]
+    i=$i+1 
 
-	read v[0]
-  i=$i+1 
-
-	echo "doriti sa mai introduceti o valoare (1 daca da, altceva daca nu)?"
-  read raspuns
-
-	while [ $raspuns -eq 1 ]
-  do
-    echo "introduceti o valoare: "
-    read v[i]
-    i=$i+1
-    
-    echo "doriti sa mai introduceti o valoare( 1 daca da altaceva daca nu )?"
+    echo -n "doriti sa mai introduceti o valoare (1 daca da, altceva daca nu)?: "
     read raspuns
-  done
+
+    while [ $raspuns -eq 1 ]
+    do
+        echo -n "introduceti o valoare: "
+        read v[i]
+        i=$i+1    
+        echo -n "doriti sa mai introduceti o valoare( 1 daca da altaceva daca nu )?: "
+        read raspuns
+    done
  
-	n=$i
-	echo "vector final: ${v[@]}"
-  return ${v[@]}
+    n=$i
+	
+    #echo "${v[@]}"
+    
+    return 10
 }
 
 
 max_vect() {
+    echo in max_vect: ${v[@]}
+    max=${v[0]}
+    
+    for i in ${v[@]}
+    do
+        echo Valoarea din vector: $i
+        if [ $max -lt $i ]
+        then
+            max=$i
+        fi
+    done
 
-	max = $1[0]
-  echo "max = $max"
+    return 0
 }
 
-v=`creare_vect`
-#vector=$(creare_vect)
+creare_vect
 
-#echo ${v[@]}
+# salvare cod de return - ce s-a dat ca parametru la return
+REZ_CREATE=$?
+echo rezultat returnat: $REZ_CREATE
+
+# vectorul v este variabila globala, vizibila in afara functiei
+echo VECTOR: ${v[@]}
+
+# vectorul v este vizibil si in interiorul functiei max_vect
+max_vect
+# dupa executia functiei max_vect, max este vizibil si in afara functiei
+echo max = $max
